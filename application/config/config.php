76197aa788ff7051fff9f550eef51bd8cb014739
Romy John <romyjohn12@gmail.com>
2022-01-31 12:40:33 +0530

    Project ready to run

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'https://receipt.32watts.com/';
+#$config['base_url'] = 'https://receipt.32watts.com/';
+$config['base_url'] = 'http://localhost/Payment_Receipts/';
 
 /*
 |--------------------------------------------------------------------------
